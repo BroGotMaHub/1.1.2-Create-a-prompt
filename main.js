a54b90cfd8c5f6e2613ea25f5f17199e9e7d930a
BroGotMaHub <johnisworking94124@gmail.com>
2026-05-07 03:42:19 +0000

    Prompt created

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-// Write your code for Part 1 below
-
-
-
-
-// Then, after your tried a prompt yourself,
-// uncomment the two lines below to try Part 2
-
-// var firstName = prompt('What is your name?');
-// alert('Welcome ' + firstName + '!');
+prompt('full name')
+alert('john thomas evans')
